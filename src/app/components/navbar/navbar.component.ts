d126d7df00f4693f3f88d216eace25e0c7da61c5
hsilv <hm.sebastiansilva@gmail.com>
2023-05-19 11:13:32 -0600

    "SPA y Routing"

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,17 +1,17 @@
 import { Component } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent {
-    location: string = window.location.href;
-    onLocate(linkr: string){
-      return window.location.href.includes('http://'+window.location.hostname+':'+window.location.port+'/'+linkr)
-    }
-
-    onRedir(linkr: string){
-      window.location.href = 'http://'+window.location.hostname+':'+window.location.port+'/'+linkr;
-    }
+  location: string = '';
+  constructor(private router: Router) {
+    this.location = this.router.url;
+  }
+  onLocate(linkr: string) {
+    return (this.location == linkr);
+  }
 }
