18c4a8142522b71149b9f0cac3ffc51f17417e8a
RenattoGuzman <renttogs@gmail.com>
2023-05-23 23:20:23 -0600

    componente tabla

--- src/app/components/table/table.component.ts
+++ src/app/components/table/table.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class TableComponent {
 
+  tableData: any[] = [
+    { id: 1, nombre: 'John', evolucion: 'Some evolution', estado: 'Active', genero: 'Male', edad: 25, expediente: 'ABC123' },
+    { id: 2, nombre: 'Jane', evolucion: 'Another evolution', estado: 'Inactive', genero: 'Female', edad: 30, expediente: 'DEF456' },
+    // Add more data objects as needed
+  ];
+
 }
