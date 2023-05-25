8d0af7e61b947b0df82ad5df6aace486ba3f44e6
hsilv <hm.sebastiansilva@gmail.com>
2023-05-25 09:52:48 -0600

    "Agregación de alerta a Inventario"

--- src/app/pages/inventory/inventory.component.ts
+++ src/app/pages/inventory/inventory.component.ts
@@ -3,6 +3,8 @@ import { getDocInfo, getPatients } from 'src/app/service/home';
 const ip='http://127.0.0.1:3000/';
 import { fetchSpecialties } from 'src/app/service/register';
 import { getMedicalCenter, getStock } from 'src/app/service/inventory';
+const limit = new Date('2023-05-05');
+const all = 350;
 
 @Component({
   selector: 'app-inventory',
@@ -16,6 +18,8 @@ export class InventoryComponent implements OnInit{
   centerName: string = "";
   centerId: number = 99999999;
   data: (number | string)[][] = [];
+  displayAlert: boolean = false;
+  alertText: string[] = ['Se sugiere comprar los siguientes medicamentos: '];
 
   constructor(){
     this.username = localStorage.getItem('medUser')!;
@@ -28,6 +32,18 @@ export class InventoryComponent implements OnInit{
     this.centerName = center.center_name;
     this.centerId = center.id_center;
     this.data = <(number | string)[][]> await getStock(this.centerId, this.token); 
+    var perc = (15/100)*all;
+    for(let row of this.data){
+      row.forEach((value, index, array) => {
+        if(index === 2 && <number>value < perc){
+          this.displayAlert = true;
+          this.alertText.push("\n"+array[1]+" (Por cantidad)")
+        } else if (index === 3 && new Date(value) < limit){
+          this.displayAlert = true;
+          this.alertText.push("\n"+array[1]+' (Por fecha de vencimiento)');
+        }
+      })
+    }
   }
 
 }
